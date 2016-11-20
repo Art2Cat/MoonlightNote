2b01b47d66fadc638ef5b8612b2b53379b28a268
art2cat <yiming.whz@gmail.com>
2016-11-20 22:18:13 +0800

    修复bug

--- app/src/main/java/com/art2cat/dev/moonlightnote/Controller/Login/LoginActivity.java
+++ app/src/main/java/com/art2cat/dev/moonlightnote/Controller/Login/LoginActivity.java
@@ -2,8 +2,8 @@ package com.art2cat.dev.moonlightnote.Controller.Login;
 
 import android.Manifest;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -19,22 +19,20 @@ import com.art2cat.dev.moonlightnote.Utils.Firebase.DatabaseUtils;
 import com.art2cat.dev.moonlightnote.Utils.PermissionUtils;
 import com.art2cat.dev.moonlightnote.Utils.SPUtils;
 import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FileDownloadTask;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 import java.io.File;
+import java.util.List;
 
+import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
 
+import static com.art2cat.dev.moonlightnote.Model.Constants.STORAGE_PERMS;
 import static com.google.firebase.auth.FirebaseAuth.getInstance;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
     private static final String TAG = "LoginActivity";
     private static final String AD_UNIT_ID = "ca-app-pub-5043396164425122/9918900095";
     private FragmentManager mFragmentManager;
@@ -42,13 +40,13 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private DatabaseUtils mDatabaseUtils;
+    private Fragment mFragment;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
 
         //初始化Admob
         MobileAds.initialize(this, AD_UNIT_ID);
@@ -58,45 +56,10 @@ public class LoginActivity extends AppCompatActivity {
         if (!flag) {
             signIn();
         }
+        startAdFragment();
 
-        String perm = Manifest.permission.WRITE_EXTERNAL_STORAGE;
-        if (!EasyPermissions.hasPermissions(this, perm)) {
-            PermissionUtils.requestStorage(this, perm);
-            return;
-        }
+        requestPermission();
 
-        mFragmentManager = getSupportFragmentManager();
-        Fragment fragment = mFragmentManager.findFragmentById(R.id.login_container);
-
-        //这里判断是否是重新登陆，如果是，则直接进入登陆界面，如果不是则，加载广告页面
-        boolean reLogin = getIntent().getBooleanExtra("reLogin", false);
-        if (reLogin) {
-            if (fragment == null) {
-                fragment = new LoginFragment();
-                mFragmentManager.beginTransaction()
-                        .add(R.id.login_container, fragment)
-                        .commit();
-            }
-        } else {
-            //在这里首先加载一个含有广告的fragment
-            if (fragment == null) {
-
-                fragment = new AdFragment();
-                mFragmentManager.beginTransaction()
-                        .add(R.id.login_container, fragment)
-                        .commit();
-            }
-
-            //创建handler对象，调用postDelayed()方法，启动插播5秒广告
-            Handler handler = new Handler();
-            handler.postDelayed(new UpdateUI(), 5000);
-        }
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @Override
@@ -153,23 +116,89 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    private void downloadUserConfig(String userId) {
-        Uri file = Uri.fromFile(new File(getFilesDir().getPath() + "/UserConfig.json"));
-        StorageReference storageReference = FirebaseStorage.getInstance()
-                .getReferenceFromUrl(Constants.FB_STORAGE_REFERENCE)
-                .child(userId).child("userconfig").child("UserConfig.json");
+    private void startAdFragment() {
+        mFragmentManager = getSupportFragmentManager();
+        Fragment fragment = mFragmentManager.findFragmentById(R.id.login_container);
+        //这里判断是否是重新登陆，如果是，则直接进入登陆界面，如果不是则，加载广告页面
+        boolean reLogin = getIntent().getBooleanExtra("reLogin", false);
+        if (reLogin) {
+            if (fragment == null) {
+                fragment = new LoginFragment();
+                mFragmentManager.beginTransaction()
+                        .add(R.id.login_container, fragment)
+                        .commit();
+            }
+        } else {
+            //在这里首先加载一个含有广告的fragment
+            if (fragment == null) {
 
-        storageReference.getFile(file).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-                Log.d(TAG, "onSuccess: ");
+                fragment = new AdFragment();
+                mFragmentManager.beginTransaction()
+                        .add(R.id.login_container, fragment)
+                        .commit();
             }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.w(TAG, "onFailure: " + e.toString());
+
+        }
+
+    }
+
+    @AfterPermissionGranted(STORAGE_PERMS)
+    private void requestPermission() {
+        String perm = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+        if (!EasyPermissions.hasPermissions(this, perm)) {
+            PermissionUtils.requestStorage(this, perm);
+        } else {
+            startLoginFragment();
+        }
+    }
+
+    private void startLoginFragment() {
+        //创建handler对象，调用postDelayed()方法，启动插播5秒广告
+        Handler handler = new Handler();
+        handler.postDelayed(new UpdateUI(), 5000);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // EasyPermissions handles the request result.
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+        if (requestCode == STORAGE_PERMS) {
+            Log.d(TAG, "onPermissionsGranted: ");
+            startLoginFragment();
+
+            File dir = new File(Environment.getExternalStorageDirectory() + "/MoonlightNote/.image");
+            File dir1 = new File(Environment.getExternalStorageDirectory() + "/MoonlightNote/.audio");
+            if (!dir.exists() || !dir1.exists()) {
+                if (dir.mkdirs()) {
+                    Log.d(TAG, "/MoonlightNote/.image: created");
+                }
+
+                if (dir1.mkdirs()) {
+                    Log.d(TAG, "/MoonlightNote/.audio: created");
+                }
             }
-        });
+        }
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+        if (requestCode == STORAGE_PERMS) {
+            Log.d(TAG, "onPermissionsDenied: ");
+            mFragmentManager.beginTransaction().remove(mFragment).commitAllowingStateLoss();
+            onBackPressed();
+        }
+    }
+
+    public void onBackPressed() {
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
     }
 
     /**
@@ -189,12 +218,11 @@ public class LoginActivity extends AppCompatActivity {
                 //这里调用Activity.finish()方法销毁当前Activity
                 finish();
             } else {
-                Fragment fragment = new LoginFragment();
+                mFragment = new LoginFragment();
                 mFragmentManager.beginTransaction()
-                        .replace(R.id.login_container, fragment)
+                        .replace(R.id.login_container, mFragment)
                         .commitAllowingStateLoss();
             }
         }
-
     }
 }
