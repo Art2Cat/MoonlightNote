030999e81a5543ec6fe43ab0471284534491cd8c
art2cat <yiming.whz@gmail.com>
2016-12-16 00:16:44 +0800

    fix AES encrypt bug, minor change(to do: add SettingsFragment more settings, add ImageFragment scale image)

--- app/src/main/java/com/art2cat/dev/moonlightnote/CustomView/CustomBehavior/ScaleDownShowBehavior.java
+++ app/src/main/java/com/art2cat/dev/moonlightnote/CustomView/CustomBehavior/ScaleDownShowBehavior.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2016 Yan Zhenjie
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.art2cat.dev.moonlightnote.CustomView.CustomBehavior;
 
 import android.content.Context;
@@ -23,13 +9,6 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-/**
- * <p>下拉时显示FAB，上拉隐藏，留出更多位置给用户。</p>
- * Created on 2016/7/14.
- *
- * @author Yan Zhenjie.
- */
 public class ScaleDownShowBehavior extends FloatingActionButton.Behavior {
 
     private OnStateChangedListener mOnStateChangedListener;
